ccb84eecbcb76dbfd8aa67a1565826710d89c840
ras <ras@nuc.(none)>
2014-10-13 13:44:28 +0200

    Created QTproject from CMakeFile and added ignore file

---
+++ src/hand_tracker.cpp
@@ -0,0 +1,15 @@
+#include <ros/ros.h>
+//#include <sensor_msgs/Image.h>
+//#include <cv_bridge/cv_bridge.h>
+//#include <opencv2/imgproc/imgproc.hpp>
+//#include <opencv2/highgui/highgui.hpp>
+
+//void depthImageCallback(const sensor_msgs::Image& img) {
+
+//}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "hand_tracker");
+
+    return 0;
+}
